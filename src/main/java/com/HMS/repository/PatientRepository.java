6ce2b8b8f178546137bd2b93c8ec302104f442c7
Naveensankaran <sankarnaveen622@gmail.com>
2025-11-10 23:17:53 +0530

    After some updates

--- src/main/java/com/HMS/repository/PatientRepository.java
+++ src/main/java/com/HMS/repository/PatientRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import com.HMS.entity.Patient;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
@@ -20,4 +21,6 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     // 🔍 Find patients by gender
     List<Patient> findByGender(String gender);
+    
+    Optional<Patient> findByNameAndPhoneNumber(String name, String phoneNumber);
 }
